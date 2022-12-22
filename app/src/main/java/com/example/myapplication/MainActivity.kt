df18599ee71ff47993713bfcf658a03accb5ed36
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-12-22 08:12:30 +0300

    :memo: Menu Creation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import androidx.activity.viewModels
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
@@ -11,6 +15,8 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<MainViewModel>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +26,45 @@ class MainActivity : AppCompatActivity() {
 
         lifecycle.addObserver(MyObserver())
 
-        binding.fab.setOnClickListener(object: View.OnClickListener{
-            override fun onClick(view: View?) {
-                Snackbar.make(view!!, "Our message", Snackbar.LENGTH_LONG).show()
-            }
-        })
+        viewModel.info.observe(this){
+            displaySnackbar(it)
+        }
 
+        binding.fab.setOnClickListener { view ->
+            viewModel.loadData()
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_share -> handleShare()
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun handleShare(): Boolean {
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT,
+                    "I just purchased ${viewModel.info.value} bottles of olive oil"
+                )
+        }
+        startActivity(intent)
+        return true
+    }
+
+    private fun displaySnackbar(count: Int) {
+        Snackbar.make(
+            binding.root!!,
+            "Current value $count",
+            Snackbar.LENGTH_LONG
+        ).show()
     }
 }
